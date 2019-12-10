4a6ed10fcdc7ba7245dae0133b20b188d3d80cf1
Antonio Fernando Amorim <afamorim@gmail.com>
2019-12-09 22:47:58 -0300

    Merget master com criação de relatorio de faturamento médico

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,19 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
-import UserController from './app/controllers/UserController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
 
 import authMiddleware from './app/middlewares/auth';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
-
+const upload = multer(multerConfig);
 // routes.get('/', (req, res) => res.json({ message: 'Hellor Fernando' }));
 
 routes.post('/users', UserController.store);
@@ -15,5 +22,17 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
